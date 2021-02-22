0fd82c2cf48497c111c17f3d20d0edff0ef1c866
Phil Austin <paustin@eos.ubc.ca>
2021-02-21 19:32:28 -0800

    add pyngl

--- hub_image/jupyterhub_config.py
+++ hub_image/jupyterhub_config.py
@@ -8,16 +8,16 @@ c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
 c.JupyterHub.hub_ip = '0.0.0.0'
 # the hostname/ip that should be used to connect to the hub
 # this is usually the hub container's name
-c.JupyterHub.hub_connect_ip = 'jupyterhub_feb09'
+c.JupyterHub.hub_connect_ip = 'jupyterhub_feb21'
 
 # pick a docker image. This should have the same version of jupyterhub
 # in it as our Hub.
-c.DockerSpawner.image = 'phaustin/climbook:feb09'
+c.DockerSpawner.image = 'phaustin/climbook:feb21'
 notebook_dir = "/home/jovyan"
 c.DockerSpawner.notebook_dir = notebook_dir
 
 # tell the user containers to connect to our docker network
-c.DockerSpawner.network_name = 'net_feb09'
+c.DockerSpawner.network_name = 'net_feb21'
 c.DockerSpawner.volumes = {"jupyterhub-user-{username}": notebook_dir,
                             "/home/phil/repos/ClimateLaboratoryBook/shared_files": 
                             {"bind": '/home/jovyan/shared_files', "mode": "rw"},
